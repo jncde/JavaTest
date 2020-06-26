19d1b1fb8a02e10fda684a064bfb9e8de92ea961
Shu Yang <Shu.Yang@inform-software.com>
2020-06-26 23:45:31 +0200

    user anotation

--- src/main/java/Annotation/userAnotation/Unfinished.java
+++ src/main/java/Annotation/userAnotation/Unfinished.java
@@ -1,9 +1,22 @@
 package Annotation.userAnotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * purpose: create an user defined annotation
  * https://www.oracle.com/technical-resources/articles/hunter-meta1.html
  */
+@Unfinished ("Just articleware")
+@Documented
+@Retention (RetentionPolicy.RUNTIME)
+@Target ({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE,
+          ElementType.PACKAGE})
+@Inherited
 public @interface Unfinished {
 
 
@@ -13,3 +26,4 @@ public @interface Unfinished {
   String[] owners() default "";
   Priority priority() default Priority.MEDIUM;
 }
+
---
+++ src/main/java/Annotation/userAnotation/UnfinishedInheriteDemo.java
@@ -0,0 +1,9 @@
+package Annotation.userAnotation;
+
+public class UnfinishedInheriteDemo extends UnfinishedDemo{
+
+  @Override
+  public void foo () {
+    super.foo ();
+  }
+}
